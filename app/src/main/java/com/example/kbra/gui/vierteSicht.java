cdf680af6841a60bf5f74f27aa74e443450bbc49
Kuebra <Kuebra>
2016-11-09 10:51:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kbra/gui/vierteSicht.java
@@ -0,0 +1,13 @@
+package com.example.kbra.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vierteSicht extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vierte_sicht);
+    }
+}
---
+++ app/src/main/java/com/example/kbra/gui/zweiteSicht.java
@@ -0,0 +1,13 @@
+package com.example.kbra.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zweiteSicht extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zweite_sicht);
+    }
+}
